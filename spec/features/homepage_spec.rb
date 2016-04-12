76b08dc82ef0324f3d33cf555dc25c7f254af7c5
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-12 15:14:22 -0600

    added bootstrap-generators gem

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,17 +7,17 @@ describe "homepage" do
   end
   it "has a link" do
     visit '/'
-    expect(page).to have_link 'Begin'
+    expect(page).to have_link 'Log In'
   end
 
   it "has a link that goes to the overview page" do
     visit '/'
-    click_link 'Begin'
+    click_link ''
     expect(page).to have_content 'Missing People'
   end
 
   it "has a link" do
     visit '/'
-    expect(page).to have_link 'About-Us'
+    expect(page).to have_link 'About'
   end
 end
