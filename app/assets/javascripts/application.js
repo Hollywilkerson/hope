1b811a16cbdaea84e53264f9901a01f1ddf37a44
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-14 21:48:11 -0600

    initial setup of google map gem [#117670973]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require underscore
+//= require gmaps/google
+
+
+
+handler = Gmaps.build('Google');
+handler.buildMap({ internal: {id: 'basic_map' }});
+
+
+
+var handler = Gmaps.build('Google');
+handler.buildMap({ internal: {id: 'multi_markers'}}, function(){
+  var markers = handler.addMarkers([
+    { lat: 43, lng: 3.5},
+    { lat: 45, lng: 4},
+    { lat: 47, lng: 3.5},
+    { lat: 49, lng: 4},
+    { lat: 51, lng: 3.5}
+  ]);
+  handler.bounds.extendWith(markers);
+  handler.fitMapToBounds();
+});
--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,8 +1,12 @@
 class OverviewController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_signed_in?
+  before_action :current_user
+  before_action :user_session
 
-    before_action :authenticate_user!
-    before_action :user_signed_in?
-    before_action :current_user
-    before_action :user_session
-
+#  @users = User.all
+ # @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+ #   marker.lat user.latitude
+  #  marker.lng user.longitude
+#  end
 end
