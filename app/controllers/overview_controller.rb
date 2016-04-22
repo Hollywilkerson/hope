fbfa460da9ea833c7df9f78d547329b0767f0251
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-22 09:40:05 -0600

    changing pins to denver

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,14 +1,14 @@
 class OverviewController < ApplicationController
   before_action :authenticate_user!
-  @missing_people = [
-    {lat: 40, lng: -105},
-    {lat: 39.7392, lng: -104.991},
-    {lat: 38.125, lng: -103.11},
-    {lat: 39.2555, lng: -103.13},
-    {lat: 38.17, lng: -105.111},
-    {lat: 39.71192, lng: -103.98}
-  ]
+
+  def index
+    @missing_people = [
+      { lat: 40, lng: -105},
+      { lat: 39.7392, lng: -104.991},
+      { lat: 38.125, lng: -103.11},
+      { lat: 39.2555, lng: -103.13},
+      { lat: 38.17, lng: -105.111},
+      { lat: 39.71192, lng: -103.98}
+    ]
+  end
 end
-
-
-
