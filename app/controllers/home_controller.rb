7d7480139084bd62bc3c20a3c15c237a9cec9702
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-21 22:05:59 -0600

    adding pics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
   def index
     @missing_people = [
-      {lat: 43, lng: 3.5},
-      {lat: 45, lng: 4},
-      {lat: 47, lng: 3.5},
-      {lat: 49, lng: 4},
-      {lat: 51, lng: 3.5}
+      {lat: 40, lng: -105},
+      {lat: 39.7392, lng: -104.991},
+      {lat: 38.125, lng: -103.11},
+      {lat: 39.2555, lng: -103.13},
+      {lat: 38.17, lng: -105.111},
+      {lat: 39.71192, lng: -103.98}
     ]
   end
-
+end
 
 #  @users = User.all
 #  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
 #    marker.lat user.latitude
 #    marker.lng user.longitude
 #  end
-end
