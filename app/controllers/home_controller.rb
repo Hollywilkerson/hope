9cfe48dc58528ee76febed979496afc4071aced6
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-22 11:04:58 -0600

    Revert "added hashes to colorado"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    @missing_people = [
+      {lat: 40, lng: -105},
+      {lat: 39.7392, lng: -104.991},
+      {lat: 38.125, lng: -103.11},
+      {lat: 39.2555, lng: -103.13},
+      {lat: 38.17, lng: -105.111},
+      {lat: 39.71192, lng: -103.98}
+    ]
   end
 end
+
+#  @users = User.all
+#  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+#    marker.lat user.latitude
+#    marker.lng user.longitude
+#  end
