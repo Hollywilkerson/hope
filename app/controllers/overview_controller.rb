4ec0c6f34f3506b89c803ec56367cb8309fa9ac5
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-22 13:52:04 -0600

    reset migration and seed file

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,9 +2,6 @@ class OverviewController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
-
-
     @missing_people = Person.all
     @hash = Gmaps4rails.build_markers(@missing_people) do |person, marker|
       marker.lat person.lat
