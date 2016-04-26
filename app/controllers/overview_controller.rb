c95022568e84cc86d49c64c79ed347178b480292
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-04-26 10:39:10 -0600

    adding more traits to seed file, adding infowindow on google maps

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,26 +2,13 @@ class OverviewController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @missing_people = Person.all
+    @missing_people = Person.all.take(5)
     @hash = Gmaps4rails.build_markers(@missing_people) do |person, marker|
-      #marker.infowindow render_to_string(:partial => '/overview/infowindow')
       marker.lat person.lat
       marker.lng person.long
+      marker.infowindow person.fname + " " + person.lname
     end
   end
 end
 
-#EXAMPLE
-#def index
-  #@cities = City.all
-  #@json = @cities.to_gmaps4rails do |city, marker|
-   # marker.infowindow render_to_string(:partial => "/cities/infowindow", :locals => { :city => city})
-   # marker.title "#{city.name}"
-    #marker.json({ :population => city.population})
-   # marker.picture({:picture => "http://mapicons.nicolasmollet.com/     wp-content/uploads/mapicons/shape-default/color-3875d7/shapeco     lor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshad     owstyle-dark/gradient-iphone/information.png",
-    #  :width => 32,
-     # :height => 32})
- # end
-#end
 
-#https://andyglassblog.wordpress.com/2012/07/06/google-maps-for-rails-with-gmaps4-rails-tutorial-on-how-to-post-and-filter-locations/
